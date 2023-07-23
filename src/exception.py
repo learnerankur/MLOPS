ee01ed8fbceca27cfcc1330b3c1247d47f0d221c
learnerankur <du.ankurchaudhary1994@gmail.com>
2023-07-23 21:01:17 +0530

    Train test split

--- src/exception.py
+++ src/exception.py
@@ -23,12 +23,12 @@ class CustomException(Exception):
         return self.error_message
     
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
 
-        raise CustomException(e,sys)
+#         raise CustomException(e,sys)
 
 
